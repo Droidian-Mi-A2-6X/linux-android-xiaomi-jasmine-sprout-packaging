56def5058583fab425113432fb1a88f48b9ddba6
gibcheesepuffs <shou@disroot.org>
2022-05-24 20:31:45 +0530

    Switch to cgroup-v1

--- kernel-info.mk
+++ kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.4-0
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7 console=tty0
+KERNEL_BOOTIMAGE_CMDLINE = ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 loop.max_part=7 console=tty0 systemd.unified_cgroup_hierarchy=0
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
